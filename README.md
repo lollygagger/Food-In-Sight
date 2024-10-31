4409293483836867f37d38cc1e31affdccfe44bf
Jacob Canedy <jcanedy19@gmail.com>
2024-10-31 11:39:23 -0400

    corrected main.tf / readme

--- README.md
+++ README.md
@@ -1,13 +1,41 @@
 There is another copy of this README file located on the AMI.
+
 # Food In Sight
+
 ## Setup:
-- Create an EC2 instance using the AMI
+
+- Create an EC2 instance using the AMI (Team 5 Food Insight AMI)
 - SSH into the “ec2-user” account on the EC2 instance
-- Run `aws configure` to login to your aws account from this instance using a security key pair
+- Run `aws configure` to login to your aws account from this instance using a security key pair if you do not already have a key pair follow [these instructions](#aws-configure) to create new ones.
 - Run cd Terraform_Team
 - Use Vim/vi/nano or your text editor of choice to change the security key to match your account's
-security key in "main.tf"
+  security key in "main.tf"
 - Run `terraform plan`
 - Run `terraform apply`
 - Enter "yes" to allow terraform to launch the new instance
 - To shutdown the instance started by terraform run `terraform destroy`
+
+## Aws Configure:
+
+To get the necessary key and secret after running aws configure in the ec2 instance
+
+1. Run `aws configure` in ec2 instance, if you do not have the credentials do the following
+2. Go to IAM console
+3. Click "Create User"
+   ![alt text](readme-image/image.png)
+4. Provide a name, Hit "Next"
+   ![alt text](readme-image/image2.png)
+5. Attach policies directly and choose "AdministratorAccess", then hit "Next"
+   ![alt text](readme-image/image3.png)
+6. "Create User"
+   ![alt text](readme-image/image4.png)
+7. Select Newly Created User
+   ![alt text](readme-image/image5.png)
+8. Go to Security Credentials. Hit "Create access key"
+   ![alt text](readme-image/image6.png)
+9. Select "Command Line Interface (CLI)", Confirm confirmation, hit "Next"
+   ![alt text](readme-image/image7.png)
+10. Enter a description and hit "Create Access Key"
+    ![alt text](readme-image/image8.png)
+11. Save the access key and secret access key in very very safe place or download csv. The key and secret key is what you will use as answers to the prompt after running aws configure in the ec2 instance.
+    ![alt text](readme-image/image9.png)
---
+++ deployment/main.tf
@@ -0,0 +1,184 @@
+#Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+} 
+
+locals {
+        aws_key = "JC_AWS_KEY" #CHANGE TO BE YOUR KEY
+}
+
+# 1. Create the VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+# 2. Create Public Subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.0.0/26"
+
+  tags = {
+    Name = "public_subnet_1"
+    Tier = "Public"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.0.64/26"
+
+  tags = {
+    Name = "public_subnet_2"
+    Tier = "Public"
+  }
+}
+
+# 3. Create Private Subnets
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.0.128/26"
+
+  tags = {
+    Name = "private_subnet_1"
+    Tier = "Private"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.0.192/26"
+
+  tags = {
+    Name = "private_subnet_2"
+    Tier = "Private"
+  }
+}
+
+# 4. Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+# 5. Create Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
+# Associate Public Route Table with Public Subnets
+resource "aws_route_table_association" "public_rt_route_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_route_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# 6. Allocate Elastic IP for NAT Gateway
+resource "aws_eip" "nat_elastic_ip" {
+  domain = "vpc"
+
+  tags = {
+     Name = "Nat Gateway EIP"
+  }
+}
+
+# 7. Create NAT Gateway in the first public subnet
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_elastic_ip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "main_nat_gateway"
+  }
+}
+
+# 8. Create Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_route_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# 9. Create a Security Group for the EC2 Instance
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2_security_group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH from anywhere"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP from anywhere"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "ec2_security_group"
+  }
+}
+
+# 10. Create an EC2 Instance in the Public Subnet
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (change as needed)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet_1.id
+  security_groups = [aws_security_group.ec2_security_group.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Team Terraform Activity"
+  }
+
+  key_name = "${local.aws_key}"
+}
