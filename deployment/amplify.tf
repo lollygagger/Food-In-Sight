4ecd365c4f79ed0da0854a0b6049c39482914752
Lollygagger <ubercharge60@gmail.com>
2024-11-09 19:01:02 -0500

    Added terraform script for creating amplify

---
+++ deployment/amplify.tf
@@ -0,0 +1,25 @@
+resource "aws_amplify_app" "food-in-sight-deploy" {
+  name       = "food-in-sight"
+  repository = "https://github.com/SWEN-514-FALL-2024/term-project-2241-swen-514-05-team5"
+
+  # The default build_spec added by the Amplify Console for React.
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - cd food-in-sight
+            - npm install
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: food-in-sight/dist
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+}
