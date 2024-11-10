97ceef94ad287ccf1ab80b3b058a5d3a16c4f7df
Lollygagger <ubercharge60@gmail.com>
2024-11-10 18:05:06 -0500

    Trying yet another solution for region variable

--- deployment/main.tf
+++ deployment/main.tf
@@ -3,6 +3,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#possibly needed for easy reference of
+data "aws_region" "current" {}
+
 locals {
         aws_key = "mainusa1" #CHANGE TO BE YOUR KEY
 }
