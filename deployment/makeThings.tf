334e6c386053f8592fd946436369e08beb650a81
charlie L <cjl7292@rit.edu>
2024-11-11 02:00:32 -0500

    aws_s3_bucket_acl

--- deployment/makeThings.tf
+++ deployment/makeThings.tf
@@ -330,5 +330,18 @@ resource "aws_iam_role_policy" "apigateway_policy" {
 
 resource "aws_s3_bucket" "image_bucket" {
   bucket = "imagebucketuniquename123123089658970"
+}
+
+resource "aws_s3_bucket_ownership_controls" "image_bucket_controls" {
+  bucket = aws_s3_bucket.image_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "image_bucket_ac1" {
+  depends_on = [aws_s3_bucket_ownership_controls.image_bucket_controls]
+
+  bucket = aws_s3_bucket.image_bucket.id
   acl    = "private"
 }
