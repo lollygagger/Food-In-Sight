1f2a14fbe8ffaa63c65afb9e15715605bf67daa5
beegrass <vincent.please@gmail.com>
2024-11-11 19:21:07 -0500

    updated main

--- db/main.tf
+++ db/main.tf
@@ -73,10 +73,35 @@ resource "aws_api_gateway_rest_api" "user_api" {
     body = data.local_file.api_swagger_spec.content
 }
 
+resource "aws_api_gateway_resource" "proxy" {
+    rest_api_id = "${aws_api_gateway_rest_api.user_api.id}"
+    parent_id   = "${aws_api_gateway_rest_api.user_api.root_resource_id}"
+    path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+    rest_api_id   = "${aws_api_gateway_rest_api.user_api.id}"
+    resource_id   = "${aws_api_gateway_resource.proxy.id}"
+    http_method   = "ANY"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+    rest_api_id = "${aws_api_gateway_rest_api.user_api.id}"
+    resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+    http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+    integration_http_method = "POST"
+    type                    = "AWS_PROXY"
+    uri                     = "${aws_lambda_function.user_processing_lambda.invoke_arn}"
+}
+
 # Deploy the API Gateway
 resource "aws_api_gateway_deployment" "user_api_deployment" {
-    depends_on = [aws_api_gateway_rest_api.user_api]  # Ensure this only deploys once API is created
-    rest_api_id = aws_api_gateway_rest_api.user_api.id
+    depends_on = [
+        aws_api_gateway_rest_api.user_api
+    ]
+    rest_api_id = "${aws_api_gateway_rest_api.user_api.id}"
     stage_name  = "prod"  # Adjust stage name as needed
 }
 
@@ -85,7 +110,7 @@ resource "aws_api_gateway_deployment" "user_api_deployment" {
 resource "aws_lambda_permission" "api_gateway_invoke" {
     statement_id  = "AllowAPIGatewayInvoke"
     action        = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.user_processing_lambda.function_name
+    function_name = "${aws_lambda_function.user_processing_lambda.function_name}"
     principal     = "apigateway.amazonaws.com"
     source_arn    = "${aws_api_gateway_rest_api.user_api.execution_arn}/*/*"  # Allow all stages and methods
 }
