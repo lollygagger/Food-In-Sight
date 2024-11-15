40c677b57076f2185d57c98cf39e85b95c5281ac
Lollygagger <ubercharge60@gmail.com>
2024-11-14 23:49:16 -0500

    Fixing import issues with aws-exports

--- food-in-sight/src/App.tsx
+++ food-in-sight/src/App.tsx
@@ -3,8 +3,10 @@ import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsmobile from './aws-exports';
+import awsmobile from "aws-exports";
+
 import LandingPage from "./components/LandingPage.tsx";
+
 Amplify.configure(awsmobile);
 
 export default function App() {
