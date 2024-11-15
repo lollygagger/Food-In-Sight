74df4521869cd6c3eb2a1589a9c853df40d889c2
Lucille Blain <lucille_blain@live.com>
2024-11-14 19:22:08 -0500

    Added dependency nstall

--- deployment/rekognition_lambda.tf
+++ deployment/rekognition_lambda.tf
@@ -65,13 +65,22 @@ data "archive_file" "rekognition_lambda_layer_zip" {
 resource "aws_lambda_layer_version" "rekognition_lambda_layer" {
   filename            = data.archive_file.rekognition_lambda_layer_zip.output_path
   source_code_hash    = data.archive_file.rekognition_lambda_layer_zip.output_base64sha256
-  layer_name          = "rekognition_lambda_layer_dependencies"
+  layer_name          = "food_api_layer_dependencies"
   compatible_runtimes = ["python3.12"]
 
   depends_on = [data.archive_file.rekognition_lambda_layer_zip]
 }
 
+# Create Dependencies File
 
+resource "null_resource" "install_rekognition_lambda_layer_dependencies" {
+  provisioner "local-exec" {
+    command = "pip install -r lambda/rekog_lambda_function_requirements.txt -t layers/rekognition_lambda_layer/python/lib/python3.11/site-packages"
+  }
+  triggers = {
+    trigger = timestamp()
+  }
+}
 
 # IAM Role for Lambda
 resource "aws_iam_role" "rekog_lambda_exec_role" {
