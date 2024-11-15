2f119a12240ff2ba1c1cfb28aa595be5d20fef63
Lucille Blain <lucille_blain@live.com>
2024-11-14 22:08:56 -0500

    added file formating

--- deployment/rekognition_lambda.tf
+++ deployment/rekognition_lambda.tf
@@ -55,6 +55,17 @@ data "archive_file" "stop_model_zip" {
   output_path = "zipped/stop_model.zip"
 }
 
+
+# Create Dependencies File
+resource "null_resource" "install_rekognition_lambda_layer_dependencies" {
+  provisioner "local-exec" {
+    command = "pip install -r lambda/rekog_lambda_function_requirements.txt -t layers/rekognition_lambda_layer/python/lib/python3.11/site-packages"
+  }
+  triggers = {
+    trigger = timestamp()
+  }
+}
+
 # Archive Lambda Layer (dependencies for Rekognition)
 data "archive_file" "rekognition_lambda_layer_zip" {
   type        = "zip"
@@ -64,26 +75,19 @@ data "archive_file" "rekognition_lambda_layer_zip" {
     null_resource.install_food_api_lambda_layer_dependencies
   ]
 }
-# Create Lambda layer with dependencies
+
+# Create lambda layer with dependencies
 resource "aws_lambda_layer_version" "rekognition_lambda_layer" {
-  filename            = data.archive_file.rekognition_lambda_layer_zip.output_path
-  source_code_hash    = data.archive_file.rekognition_lambda_layer_zip.output_base64sha256
-  layer_name          = "food_api_layer_dependencies"
+  filename = data.archive_file.rekognition_lambda_layer_zip.output_path
+  source_code_hash = data.archive_file.rekognition_lambda_layer_zip.output_base64sha256
+  layer_name = "food_api_layer_dependencies"
+
   compatible_runtimes = ["python3.12"]
-
-  depends_on = [data.archive_file.rekognition_lambda_layer_zip]
+  depends_on = [
+    data.archive_file.rekognition_lambda_layer_zip
+  ]
 }
 
-# Create Dependencies File
-
-resource "null_resource" "install_rekognition_lambda_layer_dependencies" {
-  provisioner "local-exec" {
-    command = "pip install -r lambda/rekog_lambda_function_requirements.txt -t layers/rekognition_lambda_layer/python/lib/python3.11/site-packages"
-  }
-  triggers = {
-    trigger = timestamp()
-  }
-}
 
 # IAM Role for Lambda
 resource "aws_iam_role" "rekog_lambda_exec_role" {
@@ -170,13 +174,10 @@ resource "null_resource" "stop_model_trigger" {
   provisioner "local-exec" {
     when    = destroy  # Only during the destroy phase
     command = "aws lambda invoke --function-name ${self.triggers.function_name} output.txt"
-    on_failure = "continue"  # Continue if the command fails
   }
 
   triggers = {
     function_name = aws_lambda_function.stop_model.function_name
   }
 }
-
-# Lambda function to stop the model
 
