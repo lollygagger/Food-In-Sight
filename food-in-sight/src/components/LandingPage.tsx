08fec68aad94b1c3cdea95a909abe159ad34e3b4
charlie L <cjl7292@rit.edu>
2024-11-20 22:08:43 -0500

    ui

--- food-in-sight/src/components/LandingPage.tsx
+++ food-in-sight/src/components/LandingPage.tsx
@@ -1,4 +1,6 @@
 import './LandingPage.css';
+import { FaCloudUploadAlt } from "react-icons/fa";
+
 
 const LandingPage= () => {
     return (
@@ -9,13 +11,14 @@ const LandingPage= () => {
                 <section className="upload-section">
                     <h2>Upload Food Images</h2>
                     <input type="file" accept="image/*" />
-                    <button className="upload-button">Upload</button>
+                    <button className="upload-button">Upload <FaCloudUploadAlt style={{width:"10%"}} />
+                    </button>
                 </section>
 
                 <section className="translate-section">
                     <h2>Translate A Label</h2>
                     <input type="file" accept="image/*" />
-                    <button className="translate-label">Translate text in image</button>
+                    <button className="translate-label">Translate text in image <FaCloudUploadAlt style={{width:"10%"}} /> </button>
                 </section>
 
             </main>
