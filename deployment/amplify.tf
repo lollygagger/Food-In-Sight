dd804ef153c6a3f72a188d862d7a810ba51cbafa
Lollygagger <ubercharge60@gmail.com>
2024-12-06 15:39:33 -0500

    Cleanup with descision to go non-persistent cognito, added email verification to cognito

--- deployment/amplify.tf
+++ deployment/amplify.tf
@@ -1,16 +1,7 @@
-# # Reference the existing Cognito User Pool using a data source
-# data "aws_cognito_user_pool" "existing_user_pool" {
-#   user_pool_id = "us-east-1_ThHadgoKx"  # Use the ID from aws-exports.js
-# }
-#
-# # Reference the existing Cognito User Pool Client using a data source
-# data "aws_cognito_user_pool_client" "existing_user_pool_client" {
-#   user_pool_id = data.aws_cognito_user_pool.existing_user_pool.id
-#   client_id    = "26h3p878ul4me04toa7t0k1mk"  # Use the client ID from aws-exports.js
-# }
-
 resource "aws_cognito_user_pool" "food-in-sight-user-pool" {
   name = "food-in-sight-user-pool"
+
+  auto_verified_attributes = ["email"]
 }
 
 resource "aws_cognito_user_pool_client" "food-in-sight-user-pool-client" {
@@ -29,55 +20,9 @@ resource "aws_cognito_identity_pool" "food-in-sight-identity-pool" {
   }
 }
 
-
-# Create the amplify app with environment variables for the APIs and cognito
-# resource "aws_amplify_app" "food-in-sight-deploy" {
-#   name         = "food-in-sight"
-#   repository   = "https://github.com/SWEN-514-FALL-2024/term-project-2241-swen-514-05-team5"
-#   access_token = var.github_token
-#
-#   environment_variables = {
-#     VITE_USER_DIET_API_GATEWAY_URL  = aws_api_gateway_deployment.deployment.invoke_url
-#     VITE_API_GATEWAY_URL            = aws_api_gateway_deployment.api_deployment.invoke_url
-#     VITE_COGNITO_USERPOOL_ID        = aws_cognito_user_pool.food-in-sight-user-pool.id
-#     VITE_COGNITO_USERPOOL_CLIENT_ID = aws_cognito_user_pool_client.food-in-sight-user-pool-client.id
-#     VITE_COGNITO_IDENTITY_POOL_ID   = aws_cognito_identity_pool.food-in-sight-identity-pool.id
-#   }
-#
-#   build_spec = <<-EOT
-#     version: 1
-#     frontend:
-#       phases:
-#         preBuild:
-#           commands:
-#             - cd food-in-sight
-#             - npm install
-#         build:
-#           commands:
-#             - npm run build
-#       artifacts:
-#         baseDirectory: food-in-sight/dist
-#         files:
-#           - '**/*'
-#       cache:
-#         paths:
-#           - node_modules/**/*
-#   EOT
-#
-#   depends_on    = [
-#     aws_api_gateway_deployment.deployment,
-#     aws_api_gateway_deployment.api_deployment,
-#     aws_cognito_user_pool.food-in-sight-user-pool,
-#     aws_cognito_user_pool_client.food-in-sight-user-pool-client,
-#     aws_cognito_identity_pool.food-in-sight-identity-pool
-#   ]
-# }
-
-
 resource "aws_amplify_branch" "main" {
-  # app_id     = aws_amplify_app.food-in-sight-deploy.id
-  app_id            = "d1c2naelj7l2nf"
-  branch_name       = "API-to-Frontend"
+  app_id            = "d1c2naelj7l2nf" # Manually setting the app_id to match the existing deployed amplify app
+  branch_name       = "main"
   enable_auto_build = true
 
   environment_variables = {
